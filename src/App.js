d82a2d88ebf43a6a24fbe22a5465c64f08debf37
rehack65 <dojojesse@gmail.com>
2021-02-17 14:31:36 -0500

    npm start error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Route } from "react-router-dom";
+import { baseURL, config } from "./services";
+import DoneButton from "./components/DoneButton";
+import EditButton from "./components/EditButton";
+import Form from "./components/Form";
+import Level from "./components/Level";
+import MobileHamburger from "./components/MobileHamburger";
+import NewTaskButton from "./components/NewTaskButton";
+import Task from "./components/Task";
 function App() {
+  const [toggleFetch, setToggleFetch] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const resp = await axios.get(baseURL, config);
+      setTasks(resp.data.records);
+    };
+    getTasks();
+  }, [toggleFetch]);
+
   return (
     <div className="App">
-      <h3>
-        P2 stuff
-      </h3>
+      <Route path="/new" >
+        <Form tasks={tasks} setToggleFetch={ setToggleFetch}/>
+      </Route>
     </div>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import axios from "axios";
+import { baseURL, config } from "../services";
+function Form(props) {
+  const [task, setTask] = useState("");
+  const [exp, setExp] = useState("");
+  const [description, setDescription] = useState("");
+  const [user, setUser] = useState("");
+  const history = useHistory();
+  const params = useParams();
+
+  useEffect(() => {
+    if (props.tasks.length > 0 && params.id) {
+      const foundTask = props.tasks.find((task) => params.id === task.id);
+      if (foundTask) {
+        setTask(foundTask.fields.task);
+        setExp(foundTask.fields.exp);
+        setDescription(foundTask.fields.description);
+      }
+    }
+  }, [props.tasks, params.id]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const fields = {
+      task,
+      exp,
+      description,
+    };
+    if (params.id) {
+      const taskURL = `${baseURL}/${params.id}`;
+      await axios.put(taskURL, { fields }, config);
+    } else {
+      await axios.post(baseURL, { fields }, config);
+    }
+    props.setToggleFetch((curr) => !curr);
+    history.push("/");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="task">Task:</label>
+      <input
+        id="task"
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <label htmlFor="description">Description of task: </label>
+      <textarea
+        id="description"
+        type="text"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <label htmlFor="exp">Experince value of task: </label>
+      <input
+        id="exp"
+        type="text"
+        value={exp}
+        onChange={(e) => setExp(e.target.value)}
+      />
+      <label htmlFor="user">What is your name? </label>
+      <input
+        id="user"
+        type="text"
+        value={user}
+        onChange={(e) => setUser(e.target.value)}
+      />
+      <button type="submit">Submit Task</button>
+    </form>
+  );
+}
+
+export default Form;
